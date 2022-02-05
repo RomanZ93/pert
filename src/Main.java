85f13a5a175f24bb8416a067a5c5f0518f6c5085
Роман Журавлев <rzuravlev176@gmail.com>
2022-02-05 17:27:31 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ticket");
+
+        int price = 1000;
+        int promotion = 20;
+
+        System.out.println(price / promotion);
+
+    }
+}
